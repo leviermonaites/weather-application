dd68d5e39edcc1b5173f0a4827f7e405c9f7e2b4
Levi Ermonaites De Freitas <68472819+leviermonaites@users.noreply.github.com>
2021-05-01 09:29:21 -0300

    Finishing WeatherStatistics component

--- src/components/WeatherStatistics/index.js
+++ src/components/WeatherStatistics/index.js
@@ -16,28 +16,37 @@ const WeatherStatistics = () => {
   return (
     <div id='weather-statistics-container'>
       <h1>Weather Statistics:</h1>
-      <div>
+      <div id='weather-statistics-tables-container'>
         {statistics.length > 0
           ? statistics.map((measures) => {
               console.log(measures);
               return (
-                <table className='measures'>
-                  <tr>
-                    <th>Temperature</th>
-                    <th>Humidity</th>
-                    <th>Date</th>
-                  </tr>
-                  <tr>{measures.temperature}</tr>
-                  <tr>{measures.humidity}</tr>
-                  <tr>{measures.date}</tr>
-                  {measures.temperature}
-                </table>
+                <>
+                  <table className='measurements-record-table'>
+                    <tr className='measurements-table-headers'>
+                      <th className='measurements-table-header'>Temperature</th>
+                      <th className='measurements-table-header'>Humidity</th>
+                      <th className='measurements-table-header'>Date</th>
+                    </tr>
+                    <tr className='measurements-table-data-row'>
+                      <td className='measurements-table-data'>
+                        {measures.temperature}
+                      </td>
+                      <td className='measurements-table-data'>
+                        {measures.humidity}
+                      </td>
+                      <td className='measurements-table-data'>
+                        {measures.date}
+                      </td>
+                    </tr>
+                  </table>
+                </>
               );
             })
           : null}
       </div>
       <button id='get-weather-statistics-btn' onClick={getWeatherStatistics}>
-        Get most recent weather statistics
+        Get the most recent weather statistics
       </button>
     </div>
   );
